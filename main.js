c65c8341746f37d201dd0c382cdd0d87b2aa7ddc
lynden <millingtonlyn00@gmail.com>
2022-04-05 14:51:27 -0400

    Revert changes

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import * as THREE from './node_modules/three/';
+import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+
 var mixer, mixer2, mixer3, mixer4, mixer5, delta;
 const degree = (360/(document.documentElement.scrollHeight-document.documentElement.clientHeight));
 var clock = new THREE.Clock();
