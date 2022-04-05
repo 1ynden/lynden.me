3ebda66871e2ba2ab0519b2f7bdffdb1a889e31c
lynden <millingtonlyn00@gmail.com>
2022-04-05 14:24:54 -0400

    Moving imports

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-import * as THREE from '/node_modules/three/';
-import { GLTFLoader } from '/node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from './GLTFLoader.js';
 
 var mixer, mixer2, mixer3, mixer4, mixer5, delta;
 const degree = (360/(document.documentElement.scrollHeight-document.documentElement.clientHeight));
