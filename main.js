ca6c19421979221a4b2bd17ba42f57857eafadb3
lynden <millingtonlyn00@gmail.com>
2022-04-22 13:51:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 import * as THREE from '/node_modules/three';
 import { GLTFLoader } from '/node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+=======
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+>>>>>>> parent of de7e8e4 (Specify Nodes Folders)
 
 var mixer, mixer2, mixer3, mixer4, mixer5, delta;
 const degree = (360/(document.documentElement.scrollHeight-document.documentElement.clientHeight));
